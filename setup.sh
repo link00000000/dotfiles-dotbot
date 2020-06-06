b28bb57d22f8227521d43803a01254a7b7a28f15
link00000000 <crandall.logan@gmail.com>
2020-06-05 23:44:35 -0400

    Move change shell command out of yaml file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 sudo ./install -p dotbot-apt/apt.py -c packages.conf.yaml
+chsh -s $(which fish)
+
 ./install -c fish.conf.yaml
 ./install -c tmux.conf.yaml
 ./install -c vim.conf.yaml
