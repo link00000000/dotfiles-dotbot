cb619247635e7ae7fc1a779b96e2aac63cdd902a
link00000000 <crandall.logan@gmail.com>
2020-06-06 01:11:19 -0400

    Add tpm

--- scripts/install-tmux.sh
+++ scripts/install-tmux.sh
@@ -2,3 +2,7 @@
 
 sudo apt update -y
 sudo apt install -y tmux
+
+# Install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Install plugins by pressing ^B + I"
