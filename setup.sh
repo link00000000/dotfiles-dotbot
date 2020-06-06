a6946945a9d4bc2030a46272d05c38c6a60dda97
link00000000 <crandall.logan@gmail.com>
2020-06-06 00:39:30 -0400

    reorganizer dotfiles

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo ./install -p dotbot-apt/apt.py -c packages.conf.yaml
-chsh -s $(which fish)
-
-./install -c fish.conf.yaml
-./install -c tmux.conf.yaml
-./install -c vim.conf.yaml
