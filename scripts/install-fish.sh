4faf7c49d623e460b246461315aed3657a58c702
link00000000 <crandall.logan@gmail.com>
2020-06-10 21:40:43 -0400

    Add sudo.conf.yaml config

--- scripts/install-fish.sh
+++ scripts/install-fish.sh
@@ -4,8 +4,8 @@ sudo add-apt-repository -y ppa:fish-shell/release-3
 sudo apt update -y
 sudo apt install -y fish curl
 
-# Change default shell to fish
-chsh -s $(which fish)
+# Set default shell to fish
+chsh -s "$(which fish)"
 
 # Install fisher and packages
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
