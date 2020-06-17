c667d35995bf5e4435d785cf14a58a21c0508d1a
link00000000 <crandall.logan@gmail.com>
2020-06-17 14:11:23 -0400

    Set default browser to Windows explorer.exe

--- fish/config.fish
+++ fish/config.fish
@@ -24,3 +24,5 @@ set -Ux PYENV_ROOT $HOME/.pyenv
 set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 pyenv init - | source
 
+# have windows handle the default browser
+set -x DISPLAY "explorer.exe"
