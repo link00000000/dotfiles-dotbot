334ff45e94e0cbb69ab1c6e1e89ec221f343b753
link00000000 <crandall.logan@gmail.com>
2021-04-21 00:03:33 -0400

    Clean repository

--- scripts/install-tmux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo apt update -y
-sudo apt install -y tmux
-
-# Install tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo "Install plugins by pressing ^B + I"
